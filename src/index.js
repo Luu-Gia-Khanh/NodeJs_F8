f077369ed33c4fb90bcd9bf93261f0e0b82e4960
= <=>
2021-05-22 01:18:55 +0700

    setup nodejs 22/05/2021 1:18

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+// http logger
+app.use(morgan('combined'));
+
+// Template engine
+app.engine('hbs', handlebars({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+// set view
+app.set('views', path.join(__dirname, 'resources/views'));
+
+// ROUTE
+app.get('/trang-chu', (req, res) => {
+  res.render('home');
+});
+app.get('/tin-tuc', (req, res) => {
+  res.render('news');
+});
+
+// port
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
