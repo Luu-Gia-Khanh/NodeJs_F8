aed313c79d4af6829dbb75e6e17171fddfc3a681
= <=>
2021-05-29 17:18:05 +0700

    delete course 29/05/2021 5:17 PM

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -49,6 +49,11 @@ class CourseController{
             })
             .catch(next);
     }
+    deleteCourse(req, res, next){
+        Course.deleteOne({_id:req.params.id})
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
 }
 
 module.exports = new CourseController();
--- src/routes/course.js
+++ src/routes/course.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const courseController = require('../app/controllers/CourseController');
 
+router.delete('/delete/:id', courseController.deleteCourse);
 router.put('/processUpdataCourse/:id', courseController.processUpdataCourse);
 router.get('/update/:id', courseController.update);
 router.get('/listCourse', courseController.listCourse);
